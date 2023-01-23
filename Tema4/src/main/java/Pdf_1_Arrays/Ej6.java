bddb103a0ff86cfe205a9fec057f59ace4537004
SergioEnriquezMartinez <sergio.enrmar.1@educa.jcyl.es>
2023-01-23 12:55:06 +0100

    xd

--- Tema4/src/main/java/Pdf_1_Arrays/Ej6.java
+++ Tema4/src/main/java/Pdf_1_Arrays/Ej6.java
@@ -3,16 +3,20 @@ package Pdf_1_Arrays;
 /*Crea un programa que rellene un array de 4x4 de la siguiente manera*/
 
 public class Ej6 {
+	static final int DIAGONAL = 1;
+	static final int RELLENO = 0;
+	static final int TAMANIO = 4;
+	
 	public static void main(String[] args) {
 		
-		int [][] array4x4 = new int [4][4];
+		int [][] array4x4 = new int [TAMANIO][TAMANIO];
 		
 		for (int i = 0; i < array4x4.length; i++) {
 			for (int j = 0; j < array4x4[i].length; j++) {
 				if (i == j) {
-					array4x4 [i][j] = 1;
+					array4x4 [i][j] = DIAGONAL;
 				} else {
-					array4x4 [i][j] = 0;
+					array4x4 [i][j] = RELLENO;
 				}
 			}
 		}
