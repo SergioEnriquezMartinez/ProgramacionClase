54a0afbe7dddf406d8d167f8befc3188b3df53b6
SergioEnriquezMartinez <sergio.enrmar.1@educa.jcyl.es>
2023-02-06 13:01:53 +0100

    xd

--- Tema4/src/main/java/Pdf_1_Arrays/Ej10.java
+++ Tema4/src/main/java/Pdf_1_Arrays/Ej10.java
@@ -18,7 +18,7 @@ public class Ej10 {
 		for (int i = 0; i < array5.length; i++) {
 			for (int j = 0; j < array5[i].length; j++) {
 				if (i + j < array5.length) {
-					System.out.print(i + j + " ");
+					System.out.print((i + j) + " ");
 				} else {
 					System.out.print(((array5[i].length - 1) - i + ((array5[j].length - 1) - j)) + " ");
 				}
--- Tema4/src/main/java/Pdf_1_Arrays/Ej9.java
+++ Tema4/src/main/java/Pdf_1_Arrays/Ej9.java
@@ -18,10 +18,10 @@ public class Ej9 {
 		int [] arrayAB = new int[n * 2];
 		
 		//arrayA
-		for (int i = 0; i < arrayA.length; i++) {
+		for (int i = 0; i < arrayA.length; i++) {	//rellenamos
 			arrayA[i] = (int)((Math.random() * 100) + 1); 
 		}
-		for (int i = 0; i < arrayA.length; i++) {
+		for (int i = 0; i < arrayA.length; i++) {	//ordenamos
 			for (int j = i + 1; j < arrayA.length; j++) {
 				if (arrayA[i] > arrayA[j]) {
 					int aux = arrayA[i];
@@ -30,16 +30,16 @@ public class Ej9 {
 				}
 			}
 		}
-		for (int i = 0; i < arrayA.length; i++) {	
+		for (int i = 0; i < arrayA.length; i++) {	//imprimimos
 			System.out.print(arrayA[i] + " ");
 		}
 		System.out.println();
 		
 		//arrayB
-		for (int i = 0; i < arrayB.length; i++) {
+		for (int i = 0; i < arrayB.length; i++) {	//rellenamos
 			arrayB[i] = (int)((Math.random() * 100) + 1); 
 		}
-		for (int i = 0; i < arrayB.length; i++) {
+		for (int i = 0; i < arrayB.length; i++) {	//ordenamos
 			for (int j = i + 1; j < arrayB.length; j++) {
 				if (arrayB[i] > arrayB[j]) {
 					int aux = arrayB[i];
@@ -48,17 +48,17 @@ public class Ej9 {
 				}
 			}
 		}
-		for (int i = 0; i < arrayB.length; i++) {	
+		for (int i = 0; i < arrayB.length; i++) {	//imprimimos
 			System.out.print(arrayB[i] + " ");
 		}
 		System.out.println();
 		
 		//arrayAB
-		for (int i = 0; i < arrayA.length; i++) {
+		for (int i = 0; i < arrayA.length; i++) {	//rellenamos hasta la mitad con arraya y a partir de la mitad con arrayb
 			arrayAB[i] = arrayA[i];
 			arrayAB[arrayAB.length - 1 - i] = arrayB[i];
 		}
-		for (int i = 0; i < arrayAB.length; i++) {
+		for (int i = 0; i < arrayAB.length; i++) {	//ordenamos
 			for (int j = i + 1; j < arrayAB.length; j++) {
 				if (arrayAB[i] > arrayAB[j]) {
 					int aux = arrayAB[i];
@@ -67,7 +67,7 @@ public class Ej9 {
 				}
 			}
 		}
-		for (int i = 0; i < arrayAB.length; i++) {	
+		for (int i = 0; i < arrayAB.length; i++) {	//imprimimos
 			System.out.print(arrayAB[i] + " ");
 		}
 	}
